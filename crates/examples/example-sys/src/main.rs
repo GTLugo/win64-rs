42a0738a955a03a93b19d56364c460907059af82
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-03 00:48:40 -0400

    Renaming

--- crates/examples/example-sys/src/main.rs
+++ crates/examples/example-sys/src/main.rs
@@ -1,7 +1,7 @@
-use win64_sys::{Handle, windef::WindowId};
+use win64_sys::{Handle, windef::Window};
 
 fn main() -> anyhow::Result<()> {
-  let hwnd = unsafe { WindowId::from_raw(11) };
+  let hwnd = unsafe { Window::from_raw(11) };
   println!("Is valid? {}", unsafe { hwnd.is_window() });
   Ok(())
 }
--- crates/libs/win64-sys/src/windef.rs
+++ crates/libs/win64-sys/src/windef.rs
@@ -3,12 +3,12 @@ use windows_sys::Win32::UI::WindowsAndMessaging::IsWindow;
 use crate::{Handle, declare_handle};
 
 declare_handle!(
-  WindowId,
+  Window,
   alias = "HWND",
   doc = "https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types#hwnd"
 );
 
-impl WindowId {
+impl Window {
   /// Returns whether or not the handle identifies an existing window.
   /// # Safety
   /// A thread should not use [`WindowId::is_window`] for a window that it did not create because the window could be destroyed after this function was called. Further, because window handles are recycled the handle could even point to a different window.
@@ -21,8 +21,8 @@ impl WindowId {
 }
 
 declare_handle!(
-  InstanceId,
-  &,
+  Instance,
+  alias = "HINSTANCE",
   doc = "https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types#hinstance"
 );
 
