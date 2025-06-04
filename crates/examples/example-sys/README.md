feffcd68609b30a7a8a8d675330e59945c1208bc
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-04 00:13:58 -0400

    Removed example

--- crates/examples/example-sys/README.md
+++
@@ -1,3 +0,0 @@
-# Sample app
-
-This is just a testing crate for making sure the external api works as expected in the "real world."
--- crates/examples/example-sys/src/main.rs
+++
@@ -1,9 +0,0 @@
-use win64_sys::{Handle, processthreadsapi::StartupInfo, windef::HWindow};
-
-fn main() -> anyhow::Result<()> {
-  let info = StartupInfo::get();
-  println!("{info:?}");
-  let hwnd = unsafe { HWindow::from_raw(11) };
-  println!("Is valid? {}", unsafe { hwnd.is_window() });
-  Ok(())
-}
