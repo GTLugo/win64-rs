1ad0f89562eeb529332a5f882a7e316a2b94d4a8
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-06 00:03:39 -0400

    Update main.rs

--- crates/examples/example/src/main.rs
+++ crates/examples/example/src/main.rs
@@ -1,6 +1,10 @@
-use win64::windef::HWindow;
+use win64::{windef::HWindow, StartupInfo};
 
 fn main() -> anyhow::Result<()> {
+  let info = StartupInfo::get();
+  eprintln!("{info:?}");
+
   let hwnd = HWindow::null();
+  
   Ok(())
 }
