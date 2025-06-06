961be9bf4146b217560644ba545c8bb6586511d4
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-05 23:57:19 -0400

    StartupInfo refactor

--- crates/libs/win64/src/lib.rs
+++ crates/libs/win64/src/lib.rs
@@ -7,7 +7,6 @@
 */
 
 pub mod handle;
-pub mod processthreadsapi;
 pub mod error;
 pub mod windef;
 pub mod user;
@@ -19,7 +18,7 @@ use std::{ffi::OsString, os::windows::ffi::OsStringExt};
 
 pub use dpi;
 pub use keyboard_types;
-use windows_sys::core::{PCWSTR, PWSTR};
+use windows_sys::{core::{PCWSTR, PWSTR}, Win32::System::Threading::{GetStartupInfoW, STARTUPINFOW}};
 
 /// # Safety
 /// Must be null-terminated, valid UTF16
@@ -43,3 +42,43 @@ pub unsafe fn pwstr_to_os(s: PWSTR) -> OsString {
 pub unsafe fn pcwstr_to_os(s: PCWSTR) -> OsString {
   OsString::from_wide(unsafe { std::slice::from_raw_parts(s, wcslen(s.cast_mut())) })
 }
+
+#[derive(Debug, Clone)]
+pub struct StartupInfo {
+  pub desktop: OsString,
+  pub title: OsString,
+  pub pos: (u32, u32),
+  pub size: (u32, u32),
+  pub count_chars: (u32, u32),
+  pub fill_attribute: u32,
+  pub flags: u32,
+  pub show_window: bool,
+  pub std_input: *mut (),
+  pub std_output: *mut (),
+  pub std_error: *mut (),
+}
+
+impl StartupInfo {
+  pub fn get() -> Self {
+    let mut info = STARTUPINFOW {
+      cb: std::mem::size_of::<STARTUPINFOW>() as u32,
+      ..unsafe { std::mem::zeroed() }
+    };
+
+    unsafe { GetStartupInfoW(&mut info) };
+    Self {
+      desktop: unsafe { pwstr_to_os(info.lpDesktop) },
+      title: unsafe { pwstr_to_os(info.lpTitle) },
+      pos: (info.dwX, info.dwY),
+      size: (info.dwXSize, info.dwYSize),
+      count_chars: (info.dwXCountChars, info.dwYCountChars),
+      fill_attribute: info.dwFillAttribute,
+      flags: info.dwFlags,
+      show_window: info.wShowWindow == 0,
+      std_input: info.hStdInput.cast(),
+      std_output: info.hStdOutput.cast(),
+      std_error: info.hStdError.cast(),
+    }
+  }
+}
+
