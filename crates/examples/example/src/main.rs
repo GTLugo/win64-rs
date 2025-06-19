4a76a459559c81533d70706f4e86a06184c8ff0e
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-19 03:36:50 -0400

    Update main.rs

--- crates/examples/example/src/main.rs
+++ crates/examples/example/src/main.rs
@@ -1,4 +1,4 @@
-use win64::user::{Args, CreateWindowParams, HWindow, create_window, message::Message};
+use win64::user::{Args, CreateWindowParams, create_window, message::Message};
 
 fn main() -> anyhow::Result<()> {
   let args = Args::get();
@@ -8,17 +8,17 @@ fn main() -> anyhow::Result<()> {
 
   eprintln!("msg size: {}", size_of_val(&Message::Null));
 
-  let hwnd = HWindow::null();
-  eprintln!("HWND: {hwnd:?}");
-  eprintln!("IsWindow: {}", unsafe { hwnd.is_window() });
-
-  let window = create_window(
+  let hwnd = create_window(
     CreateWindowParams::default()
       .window_name("Window")
       .instance(Some(args.hinstance)),
   );
 
-  eprintln!("IsWindow: {:?}", window);
+  eprintln!("HWND: {hwnd:?}");
+
+  if let Ok(hwnd) = hwnd {
+    eprintln!("IsWindow: {}", unsafe { hwnd.is_window() });
+  }
 
   Ok(())
 }
