b70520d2cf4d2f5fe8395499d0e0b838e00a1c63
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-19 03:31:20 -0400

    Update lib.rs

--- crates/libs/win64/src/lib.rs
+++ crates/libs/win64/src/lib.rs
@@ -1,15 +1,14 @@
 #![cfg(target_os = "windows")]
 //! 
-//! 
-//! 
-//! 
 //! Tweaks that are made should be limited to things such as:
+//! 
 //! * Naked consts/types -> Rust new-types
 //! * C consts/enums -> Rust enums
 //! * Long parameter lists -> Structs
 //! 
 //! The goal is to keep the same general API as Win32. This allows users to keep using regular guides
 //!  and docs with minimal resistance while also benefitting from strong type-checking and clearer APIs.
+//! 
 
 pub mod core;
 pub use core::*;
