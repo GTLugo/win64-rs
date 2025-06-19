75dcc9b9cd8d2973827dbb0906ab2f798d7ce84a
Gabriel Lugo <g.alexander.lugo@gmail.com>
2025-06-19 03:57:37 -0400

    Update main.rs

--- crates/examples/example/src/main.rs
+++ crates/examples/example/src/main.rs
@@ -1,7 +1,4 @@
-use win64::{
-  convert_error,
-  user::{Args, CreateWindowError, CreateWindowParams, create_window, message::Message},
-};
+use win64::user::{Args, CreateWindowParams, create_window, message::Message};
 
 fn main() -> anyhow::Result<()> {
   let args = Args::get();
